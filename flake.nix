cc0b56cb32bdf1dfaa42dea0baa1b0f8c23b529c
Christopher Fredén <me@icetan.org>
2022-09-05 07:48:07 +0200

    Rename to upgrade-nix-daemon

--- flake.nix
+++ flake.nix
@@ -7,19 +7,21 @@
     pkgs = import nixpkgs { system = "x86_64-linux"; };
     inherit (pkgs) stdenv;
 
-    upgrade-nix = pkgs.writeShellApplication {
-      name = "upgrade-nix";
-      runtimeInputs = with pkgs; [ coreutils nix ];
-      text = builtins.readFile ./upgrade-nix;
+    upgrade-nix-daemon = pkgs.writeShellApplication {
+      name = "upgrade-nix-daemon";
+      runtimeInputs = with pkgs; [ coreutils ];
+      text = builtins.readFile ./upgrade-nix-daemon;
     };
     app = {
-      program = "${upgrade-nix}/bin/upgrade-nix";
+      program = "${upgrade-nix-daemon}/bin/upgrade-nix-daemon";
       type = "app";
     };
   in {
+    packages.x86_64-linux = {
+      default = upgrade-nix-daemon;
+    };
     apps.x86_64-linux = {
       default = app;
-      upgrade-nix = app;
     };
   };
 }
