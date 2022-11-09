78aabdaa62eb5639d8dfd9c2894d54b4b47174dc
Christopher Fredén <me@icetan.org>
2022-11-09 11:37:11 +0100

    Add support for MacOS

--- flake.nix
+++ flake.nix
@@ -1,27 +1,31 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-    inherit (pkgs) stdenv;
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) stdenv;
 
-    upgrade-nix-daemon = pkgs.writeShellApplication {
-      name = "upgrade-nix-daemon";
-      runtimeInputs = with pkgs; [ coreutils ];
-      text = builtins.readFile ./upgrade-nix-daemon;
-    };
-    app = {
-      program = "${upgrade-nix-daemon}/bin/upgrade-nix-daemon";
-      type = "app";
-    };
-  in {
-    packages.x86_64-linux = {
-      default = upgrade-nix-daemon;
-    };
-    apps.x86_64-linux = {
-      default = app;
-    };
-  };
+      upgrade-nix-daemon = pkgs.writeShellApplication {
+        name = "upgrade-nix-daemon";
+        runtimeInputs = with pkgs; [ coreutils ];
+        text = builtins.readFile ./upgrade-nix-daemon;
+      };
+      app = {
+        program = "${upgrade-nix-daemon}/bin/upgrade-nix-daemon";
+        type = "app";
+      };
+    in
+    {
+      packages = {
+        default = upgrade-nix-daemon;
+      };
+      apps = {
+        default = app;
+      };
+    });
 }
